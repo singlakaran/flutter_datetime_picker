ab37cbec846e12ae4ab1e8b420e3ce256010fe91
skaran <karan.singla@leher.ai>
2020-07-03 17:20:05 +0530

    change widget

--- lib/src/datetime_picker_theme.dart
+++ lib/src/datetime_picker_theme.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 // Migrate DiagnosticableMixin to Diagnosticable until
 // https://github.com/flutter/flutter/pull/51495 makes it into stable (v1.15.21)
 class DatePickerTheme with DiagnosticableMixin {
-  final TextStyle cancelStyle;
-  final TextStyle doneStyle;
+  final Widget cancelButton;
+  final Widget doneButton;
   final TextStyle itemStyle;
   final Color backgroundColor;
   final Color headerColor;
@@ -15,8 +15,8 @@ class DatePickerTheme with DiagnosticableMixin {
   final double itemHeight;
 
   const DatePickerTheme({
-    this.cancelStyle = const TextStyle(color: Colors.black54, fontSize: 16),
-    this.doneStyle = const TextStyle(color: Colors.blue, fontSize: 16),
+    this.cancelButton,
+    this.doneButton,
     this.itemStyle = const TextStyle(color: Color(0xFF000046), fontSize: 18),
     this.backgroundColor = Colors.white,
     this.headerColor,
